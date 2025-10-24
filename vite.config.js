78c7dbe21ea5dd267bf5fd44bb70dbaba8c0a8c4
Lycan-Xx <msbello514@gmail.com>
2025-10-24 21:05:52 +0100

    Added configuration files of replit to the project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: [
+      "3e3e3b50-d219-44c9-8dd3-075b07731bde-00-1spkrofcic6vh.riker.replit.dev",
+    ],
+  },
+});
